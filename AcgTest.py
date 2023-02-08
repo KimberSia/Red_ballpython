06cc5d1ca45b6eee76ec9d55877ebfd2ecb704f5
John Doe <johndoe@email.com>
2023-02-08 21:24:11 +0000

    training

---
+++ AcgTest.py
@@ -0,0 +1,4 @@
+#what is your name
+name = input("What is your name?")
+if len(name) >= 6
+    print("Your name is long")
--- RedMiniProject.py
+++ RedMiniProject.py
@@ -1,22 +1,23 @@
 #Create Empty List 
-AWS_List = []
+List = []
 
-#Populate List Using Append
-AWS_List.append("Ec2")
-AWS_List.append("DynamoDb")
-AWS_List.append("s3")
-AWS_List.append("Lambda")
-AWS_List.append("Cloud9")
+#Populate List Using Apppend 
+List.append("Ec2")
+List.append("DynamoDb")
+List.append("s3")
+List.append("Lambda")
+List.append("Cloud9")
 
 #Print list and length of the list
-print(AWS_List)
-print(len(AWS_List))
+print(List)
+print(len(List))
 
 #Remove 2 Specific Services From The List By Name
-AWS_List.del("Ec2")
-AWS_List.del("Lambda")
+List.remove("Ec2")
+List.remove("Lambda")
+
 
 #Print new list and new length of list
-print(AWS_List)
-print(len(AWS_List))
+print(List)
+print(len(List))
 
