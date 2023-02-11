64215a5c6536807cd0bf94c428d6048fde017fb9
Kimber.Sia <Kimsia478@gmail.com>
2023-02-11 18:58:26 +0000

    to celsius

---
+++ to-celsius.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.7
+
+#Python implementation goes here 
+
+farhrenheit = (input("What temperature (in Farhrenheit) would you like converted to Celsius? "))
+celsius = (farhrenheit -32) * 5/9
