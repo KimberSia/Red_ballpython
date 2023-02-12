5fcab27e33f4a93b196842553f2b339d4ef12ac6
Kimber.Sia <Kimsia478@gmail.com>
2023-02-12 04:24:03 +0000

    project13

--- project13.py
+++ project13.py
@@ -1,1 +1,26 @@
-#!
+#!/usr/bin/python
+
+import os
+
+path = os.getcwd()
+
+file_info = []
+
+
+for file in os.listdir():
+    
+    file_size = os.stat(file).st_size
+    
+    file_info.append({
+        
+        'name': file,
+        'size': file_size
+        
+        })
+        
+print(*file_info, sep="\n")
+    
+    
+
+
+
